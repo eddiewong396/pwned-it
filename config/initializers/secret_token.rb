53029e7497dbe6f8da9f5ea900044dc3c8ae8265
Eddie Wong <isp396@gmail.com>
2013-05-07 11:10:39 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PwnedIt::Application.config.secret_key_base = '33f993aec52555ca9cc1e009b66e52e34f7f3a7141c304faa515d7fc7a6d1853a70bd95ef0b89bb7a920c29a1086899b7daf48bac84ae5519bf131a51eb2b081'
