19d93c3e87032bc7bc46e1dce8255c9960850eae
Eddie Wong <isp396@gmail.com>
2013-05-13 08:22:46 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 PwnedIt::Application.routes.draw do
+	get 'users/new'
 	root to: 'pages#home'
 	
 	resources :posts
 	resources :users
 	
-	#get "pages/home"
-	#get "pages/about"
-	#get "pages/contact"
-	#get "pages/help"
-	
-	match '/help', to: 'pages#help',	via: 'get'
-	match '/about', to: 'pages#about',	via: 'get'
-	match '/contact', to: 'pages#contact',	via: 'get'
-	
-
-
+	match '/signup',	to: 'users#new',	via: 'get'
+	match '/help',	to: 'pages#help',	via: 'get'
+	match '/about',	to: 'pages#about',	via: 'get'
+	match '/contact',	to: 'pages#contact',	via: 'get'
 end
